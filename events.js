f7d06ac39204895c5543239bbec0a3b14c98f32a
Rowan Winsemius <rowanwins@yahoo.com.au>
2019-01-31 22:46:53 +1100

    Add event config

---
+++ events.js
@@ -0,0 +1,35 @@
+const events = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "location": "",
+        "description": "A past event will happen",
+        "eventDate": "10 February 2018",
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          149.082,
+          -35.373882
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "location": "Somewhere",
+        "description": "Some event will happen",
+        "eventDate": "10 February 2019",
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          149.08293,
+          -35.373882
+        ]
+      }
+    }
+  ]
+}
