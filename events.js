bab5ab4595f6409aabe9ca60835c9db91c482109
Adam Steer <adamsteer@users.noreply.github.com>
2019-03-19 08:20:21 +1100

    Update events.js

--- events.js
+++ events.js
@@ -4,6 +4,22 @@ const events = {
     {
       "type": "Feature",
       "properties": {
+        "location": "Walt n Burley",
+        "description": "geobeersCBR!",
+        "eventDate": "21 March 2019",
+        "eventTime": "6 pm"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          149.14550,
+          -35.31041
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
         "location": "Capital Brewing Co",
         "description": "geobeersCBR - with the OpenDataCube team, and sessions at Blochaus to work up a thirst",
         "eventDate": "13 February 2019",
